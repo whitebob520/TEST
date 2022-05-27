f14feba6f233c39b274622a3174d0082ad4cb22e
whitebob520 <102523257+whitebob520@users.noreply.github.com>
2022-05-27 10:38:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,11 +1,39 @@
-export default {
-  async fetch(request, env) {
-    let url = new URL(request.url);
-    if (url.pathname.startsWith('/')) {
-      url.hostname = 'api.telegram.org'
-      let new_request = new Request(url, request);
-      return fetch(new_request);
+// export default {
+//   async fetch(request, env) {
+//     let url = new URL(request.url);
+//     if (url.pathname.startsWith('/')) {
+//       url.hostname = 'api.telegram.org'
+//       let new_request = new Request(url, request);
+//       return fetch(new_request);
+//     }
+//     return env.ASSETS.fetch(request);
+//   },
+// };
+
+const whitelist = ["/bot5275515832:"];
+const tg_host = "api.telegram.org";
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+function validate(path) {
+    for (var i = 0; i < whitelist.length; i++) {
+        if (path.startsWith(whitelist[i]))
+            return true;
     }
-    return env.ASSETS.fetch(request);
-  },
-};
+    return false;
+}
+async function handleRequest(request) {
+    var u = new URL(request.url);
+    u.host = tg_host;
+    if (!validate(u.pathname))
+        return new Response('Unauthorized', {
+            status: 403
+        });
+    var req = new Request(u, {
+        method: request.method,
+        headers: request.headers,
+        body: request.body
+    });
+    const result = await fetch(req);
+    return result;
+}
